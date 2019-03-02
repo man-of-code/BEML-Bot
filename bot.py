b6a618a7414fb59fef7cd2e8360d2da234d71521
man-of-code <mratyunjay.soni@gmail.com>
2019-03-02 19:17:51 +0530

    Fixed bot.py

--- bot.py
+++ bot.py
@@ -57,4 +57,4 @@ def send_message(recipient_id, response):
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
-    app.run(debug = True, host = '127.0.0.1', port = 5000)
+	app.run(debug = True, host = '127.0.0.1', port = port)
