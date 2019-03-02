ba818140caf7f7c73ed207360ee1c2b616438b5d
man-of-code <mratyunjay.soni@gmail.com>
2019-03-02 19:15:51 +0530

    Changed Procfile

--- bot.py
+++ bot.py
@@ -56,5 +56,5 @@ def send_message(recipient_id, response):
     return "success"
 
 if __name__ == "__main__":
-
-    app.run(host = '127.0.0.1', port = 5000)
+	port = int(os.environ.get("PORT", 5000))
+    app.run(debug = True, host = '127.0.0.1', port = 5000)
