caa9b6da093c58b4151cc292aaa598407cf7c339
man-of-code <mratyunjay.soni@gmail.com>
2019-03-03 12:03:19 +0530

    Fixed bugs

--- msgnlp.py
+++ msgnlp.py
@@ -8,6 +8,11 @@ def process(message):
 
 	not_flag = 0 
 	working_flag = 0
+	submit_flag = 0
+	bid_flag = 0
+	confrm_flag = 0
+	status_flag = 0
+	edit_flag = 0
 
 	for word in keywords:
 		word = word.lower()
@@ -22,19 +27,38 @@ def process(message):
 
 		for key in queries:
 			if key == word:
+				if key in solutions.keys():
+					return solutions[key]
+				if key == 'bid':
+					bid_flag = 1
+				if key == 'submit':
+					submit_flag = 1
 				if key == 'tenders':
 					return solutions['all tenders']
+				if key == 'confirmation':
+					return confrm_flag = 1
+				if key == 'status':
+					return status_flag = 1
 				if key == 'tender':
 					return solutions['particular tender']
 				if key == 'not':
 					not_flag = 1
 				if key == 'working' :
 					working_flag = 1
-				else :
-					return solutions[key]
+				if key == 'edit':
+					edit_flag = 1
 
 		if not_flag == 1 and working_flag == 1 :
 			return solutions['working']
+
+		if bid_flag == 1 and submit_flag == 1:
+			return solutions['submit bid']
+
+		if bid_flag == 1 and confrm_flag == 1 and status_flag == 1:
+			return solutions['confirm bid status']
+
+		if bid_flag == 1 and edit_flag == 1:
+			return solutions['edit bid']
 
 		'''for key in vendor_list:
 			if key == word:
