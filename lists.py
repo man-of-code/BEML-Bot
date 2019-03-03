96925afc80c01ca1df3f658aec9906ba0888726f
man-of-code <mratyunjay.soni@gmail.com>
2019-03-03 11:34:27 +0530

    Fixed bugs

--- lists.py
+++ lists.py
@@ -4,10 +4,10 @@ solutions = {
 	'browser' : 'Always use Internet Explorer and run as administrator.',
 	'vendor' : 'recognized vendor',
 	'particular tender' : 'After logging in, wait till all the tenders are loaded. Then, click on the desired tender and proceed according to the tutorial website www.xyz.com.',
-	'all tenders' : 'Clock on "Even" status, and then select CurrentRFx(leaving all the fields blank). Then click "Apply".'
+	'all tenders' : 'Clock on "Even" status, and then select CurrentRFx(leaving all the fields blank). Then click "Apply".' ,
 	'hi' : "Greeting from BEML SRM. How may I help you?",
 	'website' : 'Please visit https://bemlindia.in',
-	'beml' : 'Bharat Earth Movers Limited, now known as BEML, is an Indian public sector undertaking, with headquarters in Bengaluru. It manufactures a variety of heavy equipment, such as that used for earth moving, transport and mining. Visit https://en.wikipedia.org/wiki/Bharat_Earth_Movers for more info',
+	'beml' : 'Bharat Earth Movers Limited, now known as BEML, is an Indian public sector undertaking, with headquarters in Bengaluru. It manufactures a variety of heavy equipment, such as that used for earth moving, transport and mining. Visit https://en.wikipedia.org/wiki/Bharat_Earth_Movers for more info',1
 	'ceo' : 'Shri D K Hota has assumed charge as Chairman & Managing Director with effect from 01.07.2016. He joined the Board of BEML Limited on 01.07.2013 as Director (Human Resources).',
 	'login' : 'Please visit https://bemlepci.bemlindia.in:50001/irj/portal. Insert correct credentials(User ID and Password)',
 	'working' : 'Restart the browser. The version should be 11 or above. All the pop-up blogs should be off and both the java should be enabled.'
@@ -33,7 +33,7 @@ queries = {
 	'beml',
 	'ceo',
 	'tender',
-	'tenders',
+	'tenders'
 }
 
 '''vendor_list = {
