5a19861e7b2f251208c62f0b0ce8ea37f17b8efa
man-of-code <mratyunjay.soni@gmail.com>
2019-03-03 11:56:36 +0530

    Added functions

--- lists.py
+++ lists.py
@@ -4,13 +4,16 @@ solutions = {
 	'browser' : 'Always use Internet Explorer and run as administrator.',
 	'vendor' : 'recognized vendor',
 	'particular tender' : 'After logging in, wait till all the tenders are loaded. Then, click on the desired tender and proceed according to the tutorial website www.xyz.com.',
-	'all tenders' : 'Clock on "Even" status, and then select CurrentRFx(leaving all the fields blank). Then click "Apply".' ,
+	'all tenders' : 'Click on "Even" status, and then select CurrentRFx(leaving all the fields blank). Then click "Apply".' ,
 	'hi' : "Greeting from BEML SRM. How may I help you?",
 	'website' : 'Please visit https://bemlindia.in',
 	'beml' : 'Bharat Earth Movers Limited, now known as BEML, is an Indian public sector undertaking, with headquarters in Bengaluru. It manufactures a variety of heavy equipment, such as that used for earth moving, transport and mining. Visit https://en.wikipedia.org/wiki/Bharat_Earth_Movers for more info',
 	'ceo' : 'Shri D K Hota has assumed charge as Chairman & Managing Director with effect from 01.07.2016. He joined the Board of BEML Limited on 01.07.2013 as Director (Human Resources).',
 	'login' : 'Please visit https://bemlepci.bemlindia.in:50001/irj/portal. Insert correct credentials(User ID and Password)',
-	'working' : 'Restart the browser. The version should be 11 or above. All the pop-up blogs should be off and both the java should be enabled.'
+	'working' : 'Restart the browser. The version should be 11 or above. All the pop-up blogs should be off and both the java should be enabled.',
+	'submit bid' : 'Fill the complete details of the particular bid. Now check --> save --> Digital signature will open, Java prompt will run. Now, select --> Encrypt bid --> Edit --> Decrypt bid --> Enter PIN for digital signature and click submit. Select appropriate token from Digital Signature prompt and then Enter the PIN. Confirm the action and then click Encrypt bid.',
+	'confirm bid status' : 'Go to the main menu, and select the particular tender. Click the "Refresh button".',
+	'edit bid' = 'To edit, Click on the reponse number --> edit --> make desired changes --> and submit it again.'
 }
 
 greetings = {
@@ -33,7 +36,12 @@ queries = {
 	'beml',
 	'ceo',
 	'tender',
-	'tenders'
+	'tenders',
+	'submit',
+	'bid',
+	'confirmation',
+	'status',
+	'edit'
 }
 
 '''vendor_list = {
